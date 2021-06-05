57c375fb8bbbe71aeab6743bcd0adf14843fd844
P1GAS <uiopkverti1@gmail.com>
2021-06-05 21:41:23 +0500

    app build with clean-webpack-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,34 @@
-const path = require('path');
-const autoprefixer = require('autoprefixer');
-const precss = require('precss');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const precss = require("precss");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  // Итак,  чтобы вебпак начал свою работу, нужно указать главный (основной) файл, который будет включать в себя все другие необходимые файлы (модули).
   entry: {
-    polyfill: 'babel-polyfill',
-    app: './js/app.js',
+    polyfill: "babel-polyfill",
+    app: "./js/app.js",
   },
-  // Также webpack рекомендует явно указывать, в какой директории находятся исходные файлы проекта (ресурсы). Для этого следует использовать свойство context:
-  context: path.resolve(__dirname, 'src'),
+
+  context: path.resolve(__dirname, "src"),
   devServer: {
-    publicPath: '/',
+    publicPath: "/",
     port: 9000,
-    contentBase: path.join(process.cwd(), 'dist'),
-    host: 'localhost',
+    contentBase: path.join(process.cwd(), "dist"),
+    host: "localhost",
     historyApiFallback: true,
     noInfo: false,
-    stats: 'minimal',
+    stats: "minimal",
     hot: true,
   },
   module: {
-    // Для того, чтобы трансформировать файл, используются специальные утилиты - загрузчики (loaders).
-    //Для любых настроек модуля вебпак используется поле module.
-    //Массив rules  внутри объекта module определяет список правил для загрузчиков.
     rules: [
       {
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: ['@babel/preset-env'],
+            presets: ["@babel/preset-env"],
           },
         },
         test: /\.js$/,
@@ -43,7 +40,7 @@ module.exports = {
             loader: MiniCssExtractPlugin.loader,
           },
           {
-            loader: 'css-loader',
+            loader: "css-loader",
 
             options: {
               importLoaders: 1,
@@ -51,7 +48,7 @@ module.exports = {
             },
           },
           {
-            loader: 'postcss-loader',
+            loader: "postcss-loader",
             options: {
               plugins: () => [precss, autoprefixer],
             },
@@ -62,29 +59,27 @@ module.exports = {
         test: /\.(png|jpe?g|gif)$/,
         use: [
           {
-            loader: 'file-loader',
+            loader: "file-loader",
             options: {
-              name: '[path][name].[ext]',
+              name: "[path][name].[ext]",
             },
           },
         ],
       },
     ],
   },
-  // Вебпак плагины используются для настройки процесса сборки.
-  //Например, плагин для минификации кода (во время сборки код подвергается очистке и минификации).
-  //Или плагин для сборки html страницы и css кода (скрипты вставляются в html, куски css собираются в один файл).
+
   plugins: [
-    new MiniCssExtractPlugin({ filename: './style.css' }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({ filename: "./style.css" }),
     new HtmlWebpackPlugin({
-      template: 'index.html',
+      template: "index.html",
     }),
   ],
-  // Кроме entry, мы можем указать поле, куда (в какой файл) собирать конечный результат. Это свойство задаётся с помощью поля output.
-  //По умолчанию, весь результирующий код собирается в папку dist.
+
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[hash].js',
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[hash].js",
   },
-  mode: 'development',
+  mode: "development",
 };
